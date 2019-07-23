88aff6f592cf27fe6a021c3376668100f7e63138
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-07-23 19:38:43 +0100

    Added logo and icons

---
+++ components/NavBar/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="conatiner">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img src="~assets/img/logo.png" />
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item">
+              <nuxt-link to="/">Home</nuxt-link>
+          </a>
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+              <nuxt-link to="/collections">Collections</nuxt-link>
+          </a>
+            <div class="navbar-dropdown">
+              <a class="navbar-item">Shoes</a>
+              <a class="navbar-item">Bags</a>
+              <a class="navbar-item">Clothes</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="navbar-end">
+          
+          
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+};
+</script>
+
+<style>
+@import "./_index.scss";
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
   ** Global CSS
   */
   css: [
-    '@/assets/global.scss',
+    '@/assets/styles/global.scss',
   ],
   /*
   ** Plugins to load before mounting the App
