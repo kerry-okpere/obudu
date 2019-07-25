8864f9edf920b398786aa98c14ac88e72992748a
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-07-25 10:29:04 +0100

    Created slider component

---
+++ components/HomeSlider/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <carousel>
+  <slide>
+    Slide 1 Content
+  </slide>
+  <slide>
+    Slide 2 Content
+  </slide>
+</carousel>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeSlider'
+}
+</script>
+
+<style>
+@import "./_index.scss";
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="container">
+    <HomeSlider />
+    
 
   </div>
 </template>
 
 <script>
-
+import HomeSlider from '../components/HomeSlider/index';
 
 export default {
   name: 'Home',
   components: {
+    HomeSlider
 
   }
 }
