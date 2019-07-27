70380aca2da303139f0587c531c509bc7e1fd2e6
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-07-27 11:23:06 +0100

    Added map component

---
+++ components/HomeMap/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="container map-container">
+    <span class="white-overlay">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15856.486928846793!2d3.3743661!3d6.5062713!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x914e07c753fd331!2sOzone+Cinemas+Sabo+Yaba+Lagos!5e0!3m2!1sen!2sng!4v1559742919245!5m2!1sen!2sng&quot; width=&quot;1440px&quot; height=&quot;600px&quot; frameborder=&quot;0&quot; style=&quot;border:0;width:1440px;height:300px&quot; allowfullscreen"></iframe>
+    </span>
+    <span>
+
+        <span class="contact-container"></span>
+    </span>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'HomeMap'
+
+}
+</script>
+
+<style>
+@import "./_index.scss";
+</style>
