8d4d7170553a95184769b305fb0f87b2044b6beb
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-07-27 14:33:24 +0100

    Modified map component

--- components/HomeMap/index.vue
+++ components/HomeMap/index.vue
@@ -1,19 +1,22 @@
 <template>
     <section class="container map-container">
-    <span class="white-overlay">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15856.486928846793!2d3.3743661!3d6.5062713!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x914e07c753fd331!2sOzone+Cinemas+Sabo+Yaba+Lagos!5e0!3m2!1sen!2sng!4v1559742919245!5m2!1sen!2sng&quot; width=&quot;1440px&quot; height=&quot;600px&quot; frameborder=&quot;0&quot; style=&quot;border:0;width:1440px;height:300px&quot; allowfullscreen"></iframe>
-    </span>
-    <span>
-
-        <span class="contact-container"></span>
-    </span>
+    <div>
+        <span class="map__overlay"></span>   
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15856.486928846793!2d3.3743661!3d6.5062713!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x914e07c753fd331!2sOzone+Cinemas+Sabo+Yaba+Lagos!5e0!3m2!1sen!2sng!4v1559742919245!5m2!1sen!2sng&quot; width=&quot;1440px&quot; height=&quot;600px&quot; frameborder=&quot;0&quot; style=&quot;border:0;width:1440px;height:300px&quot; allowfullscreen"></iframe>
+        <div class="map-container__contact">
+            <div class="map__text has-text-centered">
+            <h2 class="map__text-header">Visit Our Store</h2>
+            <p class="map__text-p">Address, Lagos, Nigeria</p>
+            <p class="map__text-primary">Get Directions</p>
+            </div>
+        </div>
+    </div>
     </section>
 </template>
 
 <script>
 export default {
     name: 'HomeMap'
-
 }
 </script>
 
