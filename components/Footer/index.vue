a4468e41092fc9df808591da743d0c78c13195fe
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-07-27 12:54:12 +0100

    Added footer component

--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -1,32 +1,41 @@
 <template>
-  <div class="container">
-    <footer class="footer">
-      <div class="columns">
-        <div class="column">
-          <div class="navbar-brand">
-            <a class="navbar-item" href="/">
-              <img src="~assets/img/logo.png" />
-            </a>
-          </div>
-        </div>
-        <div class="column">
-          <div class="footer__menu">
-            <a class="footer__item">
-              Products
-              <!-- <nuxt-link to="/">Home</nuxt-link> -->
-            </a>
-
-            <a class="footer__item">
-              Accessories
-              <!-- <nuxt-link to="/">Home</nuxt-link> -->
-            </a>
-          </div>
-        </div>
-        <div class="column">
-          <p class="copyright">Copyright 2019, MercurieMart.</p>
+  <div class="container footer-container">
+    <div class="columns">
+      <div class="column">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="/">
+            <img src="~assets/img/logo.png" />
+          </a>
         </div>
       </div>
-    </footer>
+      <div class="column">
+        <span>
+        <div class="footer-container__nav">
+          <a class="navbar-item footer__nav-item">
+            About
+          </a>
+          <a class="navbar-item footer__nav-item">
+            Payment & Delivery
+          </a>
+          <a class="navbar-item footer__nav-item">
+            FAQ's
+          </a>
+          <a class="navbar-item footer__nav-item">
+            Privacy Policy
+          </a>
+        </div>
+        </span>
+      </div>
+      <div class="column footer-container__logos">
+        <span>
+          <img class="footer__logos" src="~assets/img/footer/flutterwave.svg" alt="Flutterwave">
+          <img class="footer__logos" src="~assets/img/footer/quickteller.svg" alt="Quickteller">
+        </span>
+        <span class="footer__copyright">
+          <p>Copyright 2019, MercurieMart. All rights reserved.</p>
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
