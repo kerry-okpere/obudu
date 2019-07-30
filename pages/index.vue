58ef514fd4a45c065f0a90c4a1914bd724201e8c
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-07-30 10:54:16 +0100

    Fixed home page bugs

--- pages/index.vue
+++ pages/index.vue
@@ -4,20 +4,23 @@
     <HomeProductCategory />
     <HomeProducts />
     <div class="section-2"></div>
+    <TrialBadge />
   </div>
 </template>
 
 <script>
 import HomeSlider from '../components/HomeSlider/index';
 import HomeProducts from '../components/HomeProducts/index';
-import HomeProductCategory from '../components/HomeProductCategory';
+import HomeProductCategory from '../components/HomeProductCategory/index';
+import TrialBadge from '../components/TrialBadge/index';
 
 export default {
   name: 'Home',
   components: {
     HomeSlider,
     HomeProducts,
-    HomeProductCategory
+    HomeProductCategory,
+    TrialBadge
 
   }
 }
