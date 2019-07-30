b3b70478ebb9dd228eb963006e481ed15ac48c0a
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-07-30 10:54:02 +0100

    Added floating badge component

---
+++ components/TrialBadge/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="trial-float">
+    <h1 class="heading-5">Create a store like this</h1>
+    <p class="paragraph-3">Free trial. Instant access</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
