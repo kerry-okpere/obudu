8f389358165ccfe4c1ff27a198ceb9b2ff970049
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-07-31 15:14:20 +0100

    Renamed section hero

---
+++ components/SectionHero/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="section-hero__section">
+            <div class="section-hero__overlay"></div>
+            <div class="section-hero__container w-container">
+                <h1 class="section-hero__heading">{{ heroHeading }}</h1>
+                <h1 class="section-hero__subheading">{{ heroSubheading }}</h1>
+            </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SectionHero',
+    props: {
+        heroHeading: String,
+        heroSubheading: String
+    }
+
+}
+</script>
+
+<style>
+@import "./_index.scss";
+</style>
