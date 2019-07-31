0643be700d0f5e1a70cbf32a7f26150ab101136a
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-07-31 14:50:04 +0100

    Added collection page

---
+++ pages/collection/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <CollectionHero />
+        <CollectionProducts />
+    </div>
+</template>
+
+<script>
+
+import CollectionHero from '../../components/CollectionHero/index';
+import CollectionProducts from '../../components/CollectionProducts';
+
+export default {
+    name: 'Collections',
+    components: {
+        CollectionHero,
+        CollectionProducts
+    }
+
+}
+</script>
+
+<style>
+@import './index.scss';
+</style>
