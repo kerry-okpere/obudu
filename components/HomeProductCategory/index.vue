13718a392ead5a316fbdd6fe4dbd44fe83b42e50
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-08-08 17:52:35 +0100

    Added category titles

--- components/HomeProductCategory/index.vue
+++ components/HomeProductCategory/index.vue
@@ -1,9 +1,30 @@
 <template>
   <div class="featured-category__section w-container">
     <div class="w-layout-grid grid">
-      <div id="w-node-995953f3b94f-3cdbab3b" class="home-category__2"></div>
-      <div id="w-node-32c54463ef60-3cdbab3b" class="home-category__1"></div>
-      <div id="w-node-4981e826d649-3cdbab3b" class="home-category__3"></div>
+      <div id="w-node-995953f3b94f-3cdbab3b" class="featured-category__box">
+          <div class="featured-category__gridbox">
+          <a href="/collection" class="featured-category__image">
+          <span class="featured-category__title">Accessories</span>
+            <span class="featured-category__bg" style="background-image:url('_nuxt/assets/img/collections/image-1.jpg')"></span>
+          </a>
+          </div>
+      </div>
+      <div id="w-node-32c54463ef60-3cdbab3b" class="featured-category__box">
+          <div class="featured-category__gridbox">
+          <a href="/collection" class="featured-category__image">
+          <span class="featured-category__title">Clothing</span>
+            <span class="featured-category__bg" style="background-image:url('_nuxt/assets/img/collections/image-2.jpg')"></span>
+          </a>
+          </div>
+      </div>
+      <div id="w-node-4981e826d649-3cdbab3b" class="featured-category__box">
+          <div class="featured-category__gridbox">
+          <a href="/collection" class="featured-category__image">
+          <span class="featured-category__title">Footwear</span>
+            <span class="featured-category__bg" style="background-image:url('_nuxt/assets/img/collections/image-3.jpg')"></span>
+          </a>
+          </div>
+      </div>
     </div>
   </div>
 </template>
--- components/NavBar/index.vue
+++ components/NavBar/index.vue
@@ -1,27 +1,27 @@
 <template>
+<div>
    <div data-collapse="medium" data-animation="default" data-duration="400" class="navbar w-nav">
             <div class="w-container">
                 <div class="nav-container div-block-5">
                     <a href="/" class="brand w-nav-brand">
-                        <img src="https://uploads-ssl.webflow.com/5d2ca9e22be6bac201dbab3a/5d3ec80973102c4cf2c71eff_logo.png" width="150" alt="" class="heder-logo"/>
+                        <img src="@/assets/img/logo.png" width="150" alt="" class="heder-logo"/>
                     </a>
-                    <div class="nav__middle">
-                        <a href="#" class="nav-link w-nav-link">Shop</a>
-                        <a href="#" class="nav-link w-nav-link">Collections</a>
-                        <a href="#" class="nav-link w-nav-link">Sale</a>
-                    </div>
-                    <nav role="navigation" class="nav__right w-nav-menu">
-                        <img src="https://uploads-ssl.webflow.com/5d2ca9e22be6bac201dbab3a/5d3ec80873102c28fbc71efe_search.svg" width="15" alt="" class="image"/>
+                    <nav class="nav__middle">
+                        <nuxt-link to="/" class="nav-link w-nav-link">Shop</nuxt-link>
+                        <nuxt-link to="/" class="nav-link w-nav-link">Collections</nuxt-link>
+                    </nav>
+                    <nav class="nav__right">
+                        <img src="@/assets/img/search.svg" width="15" alt="" class="image"/>
                         <a href="#" class="nav-link nav-link__right w-nav-link">Search</a>
-                        <img src="https://uploads-ssl.webflow.com/5d2ca9e22be6bac201dbab3a/5d3ec809cb7dfd5295f7d654_cart.svg" width="15" alt="" class="image-2"/>
+                        <img src="@/assets/img/cart.svg" width="15" alt="" class="image-2"/>
                         <a href="#" class="nav-link nav-link__right w-nav-link">Cart</a>
                     </nav>
                     <div class="menu-button w-nav-button">
-                        <div class="w-icon-nav-menu"></div>
                     </div>
                 </div>
             </div>
         </div>
+</div>
 </template>
 
 <script>
