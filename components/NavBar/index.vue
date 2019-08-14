4f9e3aec8ea5c5390881e5f9c98bf1fd52c96593
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-08-14 10:50:23 +0100

    Minor modifications

--- components/NavBar/index.vue
+++ components/NavBar/index.vue
@@ -3,9 +3,9 @@
    <div data-collapse="medium" data-animation="default" data-duration="400" class="navbar w-nav">
             <div class="w-container">
                 <div class="nav-container div-block-5">
-                    <a href="/" class="brand w-nav-brand">
+                    <nuxt-link to="/" class="brand w-nav-brand">
                         <img src="@/assets/img/logo.png" width="150" alt="" class="heder-logo"/>
-                    </a>
+                    </nuxt-link>
                     <nav class="nav__middle">
                         <nuxt-link to="/" class="nav-link w-nav-link">Shop</nuxt-link>
                         <nuxt-link to="/" class="nav-link w-nav-link">Collections</nuxt-link>
@@ -14,6 +14,7 @@
                         <img src="@/assets/img/search.svg" width="15" alt="" class="image"/>
                         <a href="#" class="nav-link nav-link__right w-nav-link">Search</a>
                         <img src="@/assets/img/cart.svg" width="15" alt="" class="image-2"/>
+                        <!-- <div class="carttotal" v-if="cartCount > 0">{{ cartCount }}</div> -->
                         <a href="#" class="nav-link nav-link__right w-nav-link">Cart</a>
                     </nav>
                     <div class="menu-button w-nav-button">
@@ -25,9 +26,12 @@
 </template>
 
 <script>
+/* import { mapGetters } from "vuex";
+
 export default {
-  name: "NavBar"
-};
+  computed: {
+    ...mapGetters(["cartCount"])
+  } */
 </script>
 
 <style>
