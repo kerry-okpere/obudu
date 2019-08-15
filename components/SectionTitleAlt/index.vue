6e2f444b24bcf01f91482e94b7d21874c11648cd
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-08-15 16:24:33 +0100

    Added another section title component

---
+++ components/SectionTitleAlt/index.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="w-container section-title-alt">
+    <h1 class="section-title-alt">{{ sectionTitleAlt }}</h1>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'SectionTitleAlt',
+    props: {
+        sectionTitleAlt: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './_index.scss';
+</style>
