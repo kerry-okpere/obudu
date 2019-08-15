ce2549518f807f2f70974e86755c6c64b8e0f9a2
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-08-15 16:20:13 +0100

    Renamed footer component

--- components/NavFooter/index.vue
+++ components/NavFooter/index.vue
@@ -1,26 +1,26 @@
 <template>
-    <footer class="footer-area pt-100 pb-70">
-        <div class="container">
+    <footer class="footer-area pt-30 pb-30">
+        <div class="w-container">
             <div class="row">
                 <div class="col-sm-8 col-sm-4">
-                    <div class="copyright">
+                    <div class="footer-container">
                         <div class="footer-logo">
                             <a href="/">
                                 <img src="https://res.cloudinary.com/mercurie/image/upload/v1564583388/mercuriemart/logo-alt.png"
                                     width="150" />
                             </a>
                         </div>
-                        <p>© Copyright, <a href="#">MercurieMart</a>. All Rights Reserved</p>
-                    </div>
-                    <div class="footer-nav mb-20">
+                        <div class="footer-nav">
                         <a href="/shipping" class="footer-nav__link">Shipping &amp; Returns</a>
                         <a href="/privacy" class="footer-nav__link">Privacy Policy</a>
                         <a href="/terms" class="footer-nav__link">Terms &amp; Conditions</a>
                     </div>
-                    <div class="footer__social">
+                    <div class="footer__social mb-10">
                         <img src="https://uploads-ssl.webflow.com/5d2ca9e22be6bac201dbab3a/5d3f0e2ca5236e3187ebd4e0_facebook.png" width="20" alt="" class="footer__social-icons"> 
                         <img src="https://uploads-ssl.webflow.com/5d2ca9e22be6bac201dbab3a/5d3f0e2c238e8c1a578bc519_twitter-logo-silhouette.png" width="20" alt="" class="footer__social-icons"> 
                         <img src="https://uploads-ssl.webflow.com/5d2ca9e22be6bac201dbab3a/5d3f0e2c519b940e724f2c1f_instagram-logo.png" width="20" alt="" class="footer__social-icons">
+                    </div>
+                        <p class="copyright">© Copyright, <a href="#">MercurieMart</a>. All Rights Reserved</p>
                     </div>
                 </div>
             </div>
@@ -30,7 +30,7 @@
 
 <script>
     export default {
-        name: "Footer"
+        name: "NavFooter"
     };
 </script>
 
