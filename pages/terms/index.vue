873422cfc4ce205a97c82cc79ed77637bd4df151
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-08-15 13:23:11 +0100

    Modified alt page

--- pages/terms/index.vue
+++ pages/terms/index.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="container">
+    <div class="container-fluid container-full">
         <SectionHero heroHeading="Terms & Conditions" />
-        <div class="w-container">
-            <div class="standalone-text">
+        <div class="container">
+            <div class="text-bar">
                 <br/>
                 Nullam quis risus eget urna mollis ornare vel eu leo. Curabitur blandit tempus porttitor. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nulla vitae elit libero, a pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec ullamcorper nulla non metus auctor fringilla. Nullam id dolor id nibh ultricies vehicula ut id elit.<br/>
                 <br/>
@@ -10,6 +10,7 @@
                 <br/>
                 Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id dolor id nibh ultricies vehicula ut id elit. <br/>
                 <br/>Maecenas faucibus mollis interdum.Sed posuere consectetur est at lobortis. Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla. Maecenas sed diam eget risus varius blandit sit amet non magna.Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit. Curabitur blandit tempus porttitor. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                <br/><br/>
             </div>
         </div>
     </div>
