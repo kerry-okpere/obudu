a18c280d8d0530d83a07018a2bfc45146434320b
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-08-15 16:22:20 +0100

    Added animation

--- components/HomeProductCategory/index.vue
+++ components/HomeProductCategory/index.vue
@@ -1,27 +1,21 @@
 <template>
-  <div class="container">
+  <div class="w-container">
     <div class="row">
-      <div class="col-lg-4 col-md-4">
+      <div class="col-md-6 animated slideInUp">
           <div class="single-banner mb-30 scroll-zoom" data-sr-id="2">
             <a href="/product/">
-              <span class="home-category__title">Groceries</span>
-              <span class="home-category__bg" style="background-image: url(https://res.cloudinary.com/mercurie/image/upload/v1564579540/mercuriemart/image-5.jpg);"></span>
+             <span class="home-category__title">Clothing</span>
+             <span class="home-category__overlay"></span>
+             <img class="animated" src="@/assets/img/collections/image-2.jpg" alt="">
             </a>
           </div>
         </div>
-      <div class="col-lg-4 col-md-4">
+      <div class="col-md-6 animated slideInUp">
           <div class="single-banner mb-30 scroll-zoom" data-sr-id="2">
             <a href="/product/">
-              <span class="home-category__title">Groceries</span>
-              <img class="animated" src="@/assets/img/collections/image-2.jpg" alt="">
-            </a>
-          </div>
-        </div>
-      <div class="col-lg-4 col-md-4">
-          <div class="single-banner mb-30 scroll-zoom" data-sr-id="2">
-            <a href="/product/">
-              <span class="home-category__title">Groceries</span>
-              <img class="animated" src="@/assets/img/collections/image-2.jpg" alt="">
+              <span class="home-category__title">Accessories</span>
+                           <span class="home-category__overlay"></span>
+              <img class="animated" src="@/assets/img/collections/image-3.jpg" alt="">
             </a>
           </div>
         </div>
