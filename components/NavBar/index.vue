0841ed542aac29996b990a4f8d0f96d2d0e39af9
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-08-15 13:19:53 +0100

    Temp fix for Nav

--- components/NavBar/index.vue
+++ components/NavBar/index.vue
@@ -1,49 +1,53 @@
 <template>
-<header class="header-area header-padding-1 sticky-bar header-res-padding clearfix">
+  <header class="header-area sticky-bar clearfix">
     <div class="container">
-        <div class="row">
-            <div class="col-xl-2 col-lg-2 col-md-6 col-4">
-                <div class="logo">
-                    <a href="index.html">
-                        <img src="@/assets/img/logo.png" width="150" alt="" class="header-logo"/>
-                    </a>
-                </div>
+      <div class="row">
+        <div class="col-xl-2 col-lg-2 col-md-6 col-4">
+          <div class="logo">
+            <a href="/">
+              <img src="@/assets/img/logo.png" width="150" alt="" class="header-logo" />
+            </a>
+          </div>
+        </div>
+        <div class="col-xl-8 col-lg-8 d-none d-lg-block">
+          <div class="main-menu">
+            <nav>
+              <ul>
+                <li><a href="/shop/">Shop</a>
+                <li><a href="/collection/groceries/">Groceries</a></li>
+                <li><a href="/collection/sale/">Sale</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <div class="col-xl-2 col-lg-2 col-md-6 col-8">
+          <div class="header-right-wrap">
+            <div id="search" class="same-style header-search">
+              <a @click="searchVisible = !searchVvisible" class="search-active" href="#">
+                <img src="@/assets/img/search.svg" width="15" alt="" class="image" />
+                <p class="menu-item">Search</p>
+              </a>
+              <div v-if="searchVisible" class="search-content" style="display: block;">
+                <form id="search" action="/search">
+                  <input type="text" placeholder="Search">
+                  <button class="button-search"><i class="fa fa-search"></i></button>
+                </form>
+              </div>
             </div>
-            <div class="col-xl-8 col-lg-8 d-none d-lg-block">
-                <div class="main-menu">
-                    <nav>
-                        <ul>
-                            <li><a href="/">Shop</a>
-                            <li><a href="/category/groceries">Groceries</a></li>
-                            <li><a href="/category/sale">Sale</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-            <div class="col-xl-2 col-lg-2 col-md-6 col-8">
-                   <div class="header-right-wrap">
-                    <div class="same-style header-search">
-                        <a class="search-active" href="#"><img src="@/assets/img/search.svg" width="15" alt="" class="image"/><p class="menu-item">Search</p></a>
-                        <div class="search-content">
-                            <form action="#">
-                                <input type="text" placeholder="Search" />
-                                <button class="button-search"><img src="@/assets/img/search.svg" width="15" alt="" class="image"/></button>
-                            </form>
-                        </div> 
-                    </div>
-                    <div class="same-style cart-wrap">
-                        <button class="icon-cart">
-                            <img src="@/assets/img/cart.svg" width="15" alt="" class="image"/>
-                            <span class="count-style">2</span>
-                        </button>
-                        <div class="shopping-cart-content">
+            <div class="same-style cart-wrap">
+              <button @click="cartVisible = !cartVisible" class="icon-cart">
+                <img src="@/assets/img/cart.svg" width="15" alt="" class="image" />
+                <p class="menu-item">Cart</p>
+                <span class="count-style">2</span>
+              </button>
+              <div v-if="cartVisible" class="shopping-cart-content cart-visible">
                             <ul>
                                 <li class="single-shopping-cart">
                                     <div class="shopping-cart-img">
-                                        <a href="#"><img src="@/assets/img/cart.svg" width="15" alt="" class="image"/>Cart</a>
+                                        <a href="#"><img alt="" src="assets/img/cart/cart-1.png"></a>
                                     </div>
                                     <div class="shopping-cart-title">
-                                        <h4><a href="#">T- Shart & Jeans </a></h4>
+                                        <h4><a href="#">T- Shart &amp; Jeans </a></h4>
                                         <h6>Qty: 02</h6>
                                         <span>$260.00</span>
                                     </div>
@@ -56,7 +60,7 @@
                                         <a href="#"><img alt="" src="assets/img/cart/cart-2.png"></a>
                                     </div>
                                     <div class="shopping-cart-title">
-                                        <h4><a href="#">T- Shart & Jeans </a></h4>
+                                        <h4><a href="#">T- Shart &amp; Jeans </a></h4>
                                         <h6>Qty: 02</h6>
                                         <span>$260.00</span>
                                     </div>
@@ -74,32 +78,41 @@
                                 <a class="default-btn" href="checkout.html">checkout</a>
                             </div>
                         </div>
-                    </div>
-                </div>
             </div>
-        </div>
-        <div class="mobile-menu-area">
-            <div class="mobile-menu">
-                <nav id="mobile-menu-active">
+              <div class="mobile-menu-area">
+                <div class="mobile-menu">
+                  <nav id="mobile-menu-active">
                     <ul class="menu-overflow">
-                        <li><a href="index.html">HOME</a></li>
-                        <li><a href="shop.html">Shop</a></li>
-                        <li><a href="about.html">About us</a></li>
-                        <li><a href="contact.html">Contact</a></li>
+                      <li><a href="/">Home</a></li>
+                      <li><a href="/shop/">Shop</a></li>
+                      <li><a href="/collection/groceries/">Groceries</a></li>
+                      <li><a href="/collection/sale">Sale</a></li>
                     </ul>
-                </nav>
-            </div>
+                  </nav>
+                </div>
+              </div>
+          </div>
         </div>
+      </div>
     </div>
-</header>
+  </header>
 </template>
 
 <script>
-export default {
-  name: "NavBar"
-};
+  export default {
+    name: "NavBar",
+    data() {
+      return {
+        searchVisible: false,
+        cartVisible: false
+      }
+    },
+
+  };
+
 </script>
 
 <style lang="scss" scoped>
-@import "./_index.scss";
+  @import "./_index.scss";
+
 </style>
