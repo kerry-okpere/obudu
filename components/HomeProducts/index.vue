8085dd1ae79cd97895e89a8dc53dc33f27f4d3ef
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-08-15 16:21:34 +0100

    Added animation

--- components/HomeProducts/index.vue
+++ components/HomeProducts/index.vue
@@ -1,18 +1,25 @@
 <template>
 <div class="product-area pb-60">
-    <div class="container">
+    <div class="w-container" data-aos="fade-up"
+    data-aos-offset="150"
+    data-aos-delay="20"
+    data-aos-duration="500"
+    data-aos-easing="ease-in-out"
+    data-aos-mirror="true"
+    data-aos-once="true"
+    data-aos-anchor-placement="top-bottom">
                 <div class="row">
                     <div class="col-xl-3 col-md-6 col-lg-4 col-sm-6 col-6">
-                        <div class="product-wrap-2 mb-25 text-center">
+                        <div class="product-wrap-2 mb-25 text-center hvr-grow-shadow">
                             <div class="product-img">
                                 <a href="/product">
                                     <img class="default-img" src="@/assets/img/products/sa-prod1.png" alt="">
                                     <img class="hover-img" src="@/assets/img/products/sa-prod2.png" alt="">
                                 </a>
                                 <div class="product-action-2">
-                                    <a title="Add To Cart" href="#"><i class="fa fa-shopping-cart"></i></a>
-                                    <a title="Quick View" href="#" data-toggle="modal" data-target="#exampleModal"><i class="fa fa-eye"></i></a>
-                                    <a title="Compare" href="#"><i class="fa fa-retweet"></i></a>
+                                    <a title="Add To Cart" href="#"><ion-icon name="ios-cart"></ion-icon><h4>Add to Cart</h4></a>
+                                    <a title="Quick View" href="#" data-toggle="modal" data-target="#exampleModal"><ion-icon name="ios-eye"></ion-icon>
+                                    <h4>Quick View</h4></a>
                                 </div>
                             </div>
                             <div class="product-content-2">
@@ -27,16 +34,16 @@
                         </div>
                     </div>
                      <div class="col-xl-3 col-md-6 col-lg-4 col-sm-6 col-6">
-                        <div class="product-wrap-2 mb-25 text-center">
+                        <div class="product-wrap-2 mb-25 text-center hvr-grow-shadow">
                             <div class="product-img">
                                 <a href="/product">
                                     <img class="default-img" src="@/assets/img/products/sa-prod1.png" alt="">
                                     <img class="hover-img" src="@/assets/img/products/sa-prod2.png" alt="">
                                 </a>
                                 <div class="product-action-2">
-                                    <a title="Add To Cart" href="#"><i class="fa fa-shopping-cart"></i></a>
-                                    <a title="Quick View" href="#" data-toggle="modal" data-target="#exampleModal"><i class="fa fa-eye"></i></a>
-                                    <a title="Compare" href="#"><i class="fa fa-retweet"></i></a>
+                                    <a title="Add To Cart" href="#"><ion-icon name="ios-cart"></ion-icon><h4>Add to Cart</h4></a>
+                                    <a title="Quick View" href="#" data-toggle="modal" data-target="#exampleModal"><ion-icon name="ios-eye"></ion-icon>
+                                    <h4>Quick View</h4></a>
                                 </div>
                             </div>
                             <div class="product-content-2">
@@ -51,16 +58,16 @@
                         </div>
                     </div>   
                      <div class="col-xl-3 col-md-6 col-lg-4 col-sm-6 col-6">
-                        <div class="product-wrap-2 mb-25 text-center">
+                        <div class="product-wrap-2 mb-25 text-center hvr-grow-shadow">
                             <div class="product-img">
                                 <a href="/product">
                                     <img class="default-img" src="@/assets/img/products/sa-prod1.png" alt="">
                                     <img class="hover-img" src="@/assets/img/products/sa-prod2.png" alt="">
                                 </a>
                                 <div class="product-action-2">
-                                    <a title="Add To Cart" href="#"><i class="fa fa-shopping-cart"></i></a>
-                                    <a title="Quick View" href="#" data-toggle="modal" data-target="#exampleModal"><i class="fa fa-eye"></i></a>
-                                    <a title="Compare" href="#"><i class="fa fa-retweet"></i></a>
+                                    <a title="Add To Cart" href="#"><ion-icon name="ios-cart"></ion-icon><h4>Add to Cart</h4></a>
+                                    <a title="Quick View" href="#" data-toggle="modal" data-target="#exampleModal"><ion-icon name="ios-eye"></ion-icon>
+                                    <h4>Quick View</h4></a>
                                 </div>
                             </div>
                             <div class="product-content-2">
@@ -75,16 +82,16 @@
                         </div>
                     </div>   
                      <div class="col-xl-3 col-md-6 col-lg-4 col-sm-6 col-6">
-                        <div class="product-wrap-2 mb-25 text-center">
+                        <div class="product-wrap-2 mb-25 text-center hvr-grow-shadow">
                             <div class="product-img">
                                 <a href="/product">
                                     <img class="default-img" src="@/assets/img/products/sa-prod1.png" alt="">
                                     <img class="hover-img" src="@/assets/img/products/sa-prod2.png" alt="">
                                 </a>
                                 <div class="product-action-2">
-                                    <a title="Add To Cart" href="#"><i class="fa fa-shopping-cart"></i></a>
-                                    <a title="Quick View" href="#" data-toggle="modal" data-target="#exampleModal"><i class="fa fa-eye"></i></a>
-                                    <a title="Compare" href="#"><i class="fa fa-retweet"></i></a>
+                                    <a title="Add To Cart" href="#"><ion-icon name="ios-cart"></ion-icon><h4>Add to Cart</h4></a>
+                                    <a title="Quick View" href="#" data-toggle="modal" data-target="#exampleModal"><ion-icon name="ios-eye"></ion-icon>
+                                    <h4>Quick View</h4></a>
                                 </div>
                             </div>
                             <div class="product-content-2">
@@ -101,33 +108,6 @@
                 </div>
             </div>
         </div>
-    </div>
-</div>
-
-<!--   <div class="home-products__section w-container">
-            <h1 class="home-section-title">NEW ARRIVALS</h1>
-            <div class="w-layout-grid grid-2">
-                <div class="home-products__single">
-                    <div class="home-product-image">
-                        <div class="home-product-image__preview"><img src="@/assets/img/products/item-6.jpeg" class="loading" data-was-processed="true"></div>
-                        <div class="home-product-image__hover"><img src="@/assets/img/products/item-4.png" class="loading" data-was-processed="true"></div>
-                    </div>
-                    <div class="home-product__title">
-                        <h1 class="home-products__name"><a href="/product">Pink Pattern Dress</a></h1>
-                        <span class="home-product__price">
-                            <p class="home-products__price">₦15,000.00</p>
-                        </span>
-                    </div>
-                    <div class="home-product__buttons">
-                        <div>
-                            <a href="/cart" data-quantity="1" class="home-atc-button" rel="nofollow">Add to cart</a>
-                            <a href="http://nativewptheme.net/shop-first/wp-content/uploads/2016/11/b_1.jpg" data-thumb="http://nativewptheme.net/shop-first/wp-content/uploads/2016/11/b_1-150x150.jpg" title="" class="dfd-prod-lightbox" data-rel="prettyPhoto[product-gallery-20132]"><i class="fas fa-eye"></i></a></div>
-                    </div>
-
-                    
-                </div>                                        
-            </div>
-        </div> -->
 </template>
 
 <script>
