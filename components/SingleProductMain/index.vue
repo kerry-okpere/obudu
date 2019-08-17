135b479dd47f465dc522f744772101642d510c3c
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-08-17 09:09:55 +0100

    Modified product page

--- components/SingleProductMain/index.vue
+++ components/SingleProductMain/index.vue
@@ -1,92 +1,105 @@
 <template>
-    <div>
-        <div class="shop-area pt-100 pb-100">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-7 col-lg-7 col-md-12">
-                        <div class="product-details-img mr-20 product-details-tab">
-                            <div class="zoompro-wrap zoompro-2 pr-20">
-                                <div class="zoompro-border zoompro-span">
-                                    <img class="zoompro" src="https://res.cloudinary.com/mercurie/image/upload/v1565670635/mercuriemart/sample-2.jpg"
-                                        data-zoom-image="https://res.cloudinary.com/mercurie/image/upload/v1565670635/mercuriemart/sample-2.jpg" alt="" />
-                                </div>
-                            </div>
-                            <div id="gallery" class="product-dec-slider-2">
-                                <a data-image="https://res.cloudinary.com/mercurie/image/upload/v1565670635/mercuriemart/sample-2.jpg"
-                                    data-zoom-image="https://res.cloudinary.com/mercurie/image/upload/v1565670635/mercuriemart/sample-2.jpg">
-                                    <img src="https://res.cloudinary.com/mercurie/image/upload/v1565670635/mercuriemart/sample-1.jpg" alt="">
-                                </a>
-                                <a data-image="https://res.cloudinary.com/mercurie/image/upload/v1565671069/mercuriemart/sample-2b.jpg"
-                                    data-zoom-image="https://res.cloudinary.com/mercurie/image/upload/v1565671069/mercuriemart/sample-2b.jpg">
-                                    <img src="https://res.cloudinary.com/mercurie/image/upload/v1565671066/mercuriemart/sample-2a.jpg" alt="">
-                                </a>
-                                <a data-image="https://res.cloudinary.com/mercurie/image/upload/v1565671069/mercuriemart/sample-2b.jpg"
-                                    data-zoom-image="https://res.cloudinary.com/mercurie/image/upload/v1565671069/mercuriemart/sample-2b.jpg">
-                                    <img src="https://res.cloudinary.com/mercurie/image/upload/v1565671066/mercuriemart/sample-2a.jpg" alt="">
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-5 col-lg-5 col-md-12 hvr-shadow">
-                        <div class="product-details-content">
-                            <h2>Products Name Here</h2>
-                            <div class="product-details-price">
-                                <span>$18.00 </span>
-                            </div>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et
-                                dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco</p>
-                            <div class="pro-details-quality">
-                                <div class="cart-plus-minus">
-                                    <input class="cart-plus-minus-box" type="text" name="qtybutton" value="2">
-                                </div>
-                                <div class="pro-details-cart btn-hover">
-                                    <a href="#">Add To Cart</a>
-                                </div>
-                                <div class="pro-details-wishlist">
-                                    <a href="#"><i class="fa fa-heart-o"></i></a>
-                                </div>
-                                <div class="pro-details-compare">
-                                    <a href="#"><i class="pe-7s-shuffle"></i></a>
-                                </div>
-                            </div>
-                            <div class="pro-details-meta">
-                                <span>Categories :</span>
-                                <ul>
-                                    <li><a href="#">Minimal,</a></li>
-                                    <li><a href="#">Furniture,</a></li>
-                                    <li><a href="#">Fashion</a></li>
-                                </ul>
-                            </div>
-                            <div class="pro-details-meta">
-                                <span>Tag :</span>
-                                <ul>
-                                    <li><a href="#">Fashion, </a></li>
-                                    <li><a href="#">Furniture,</a></li>
-                                    <li><a href="#">Electronic</a></li>
-                                </ul>
-                            </div>
-                            <div class="pro-details-social">
-                                <ul>
-                                    <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                                    <li><a href="#"><i class="fab fa-whatsapp"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+  <div>
+    <div class="shop-area pt-100 pb-100">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-7 col-lg-7 col-md-12">
+            <div class="product-details-img mr-20 product-details-tab">
+              <div class="zoompro-wrap zoompro-2 pr-20">
+                <div class="zoompro-border zoompro-span">
+                  <img
+                    class="zoompro"
+                    src="@/assets/img/products/sa-prod1.png"
+                    data-zoom-image="@/assets/img/products/sa-prod1.png"
+                    alt
+                  />
                 </div>
+              </div>
+<!--               <div id="gallery" class="product-dec-slider-2">
+                <a
+                  data-image="https://res.cloudinary.com/mercurie/image/upload/v1565670635/mercuriemart/sample-2.jpg"
+                  data-zoom-image="https://res.cloudinary.com/mercurie/image/upload/v1565670635/mercuriemart/sample-2.jpg"
+                >
+                  <img
+                    src="https://res.cloudinary.com/mercurie/image/upload/v1565670635/mercuriemart/sample-1.jpg"
+                    alt
+                  />
+                </a>
+                <a
+                  data-image="https://res.cloudinary.com/mercurie/image/upload/v1565671069/mercuriemart/sample-2b.jpg"
+                  data-zoom-image="https://res.cloudinary.com/mercurie/image/upload/v1565671069/mercuriemart/sample-2b.jpg"
+                >
+                  <img
+                    src="https://res.cloudinary.com/mercurie/image/upload/v1565671066/mercuriemart/sample-2a.jpg"
+                    alt
+                  />
+                </a>
+                <a
+                  data-image="https://res.cloudinary.com/mercurie/image/upload/v1565671069/mercuriemart/sample-2b.jpg"
+                  data-zoom-image="https://res.cloudinary.com/mercurie/image/upload/v1565671069/mercuriemart/sample-2b.jpg"
+                >
+                  <img
+                    src="https://res.cloudinary.com/mercurie/image/upload/v1565671066/mercuriemart/sample-2a.jpg"
+                    alt
+                  />
+                </a>
+              </div> -->
             </div>
+          </div>
+          <div class="col-lg-5 col-lg-5 col-md-12">
+            <div class="product-details-content">
+              <h2>Organic Apple Cidar</h2>
+              <div class="product-details-price">
+                <span>$18.00</span>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisic elit eiusm tempor incidid ut labore et
+                dolore magna aliqua. Ut enim ad minim venialo quis nostrud exercitation ullamco
+              </p>
+
+              <div class="pro-details-quality">
+                <div class="pro-details-cart">
+                  <a href="#">Add To Cart</a>
+                </div>
+              </div>
+              <div class="pro-details-social">
+                <ul>
+                  <li>
+                    <a href="#">
+                      <ion-icon name="logo-facebook"></ion-icon>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <ion-icon name="logo-whatsapp"></ion-icon>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <ion-icon name="logo-instagram"></ion-icon>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <ion-icon name="logo-pinterest"></ion-icon>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "SingleProductMain",
-    };
-
+export default {
+  name: "SingleProductMain"
+};
 </script>
 
 <style lang="scss" scoped>
+@import "./_index.scss";
 </style>
