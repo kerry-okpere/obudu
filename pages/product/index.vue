13f026cc89dd10b36585a4f1a76b63a80abcb3e3
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-08-17 09:09:31 +0100

    Modified product page

--- pages/product/index.vue
+++ pages/product/index.vue
@@ -2,25 +2,23 @@
     <div>
         <SingleProductBreadcrumb />
         <SingleProductMain />
-        <SingleProductDescription />
-        <SectionTitleAlt sectionTitleAlt="Similar Products"></SectionTitleAlt>
+        <SectionTitle sectionTitle="Similar Products"></SectionTitle>
         <SingleProductSimilar />
     </div>
 </template>
 
 <script>
+
 import SingleProductBreadcrumb from '../../components/SingleProductBreadcrumb/index';
 import SingleProductMain from '../../components/SingleProductMain/index';
-import SingleProductDescription from '../../components/SingleProductDescription/index';
-import SectionTitleAlt from '../../components/SectionTitleAlt/index';
+import SectionTitle from '../../components/SectionTitle/index';
 import SingleProductSimilar from '../../components/SingleProductSimilar/index';
 
 export default {
     components: {
         SingleProductBreadcrumb,
         SingleProductMain,
-        SingleProductDescription,
-        SectionTitleAlt,
+        SectionTitle,
         SingleProductSimilar
     },
 }
