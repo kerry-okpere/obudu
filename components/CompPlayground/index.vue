be1d4b28eef19fa8dc1059bcdffdcdd268193e24
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-08-17 09:46:37 +0100

    Added playground component

---
+++ components/CompPlayground/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <SfGallery
+:sliderOptions="{ autoplay, rewind }"
+v-model="current"
+:images="images" />
+</template>
+
+<script>
+import { SfGallery } from '@storefrontui/vue'
+
+export default {
+  components: {
+    SfGallery
+  },
+
+  props: {
+      images: Array,
+  },
+
+  data () {
+      return {
+
+      }
+  },
+
+  methods: {
+      function() {
+          let images =
+           [ 
+              { small: { url, alt }, big: { url, alt }, 
+                zoom: { url, alt }
+              } 
+            ]
+     }
+  }
+}
+</script>
+
+<style>
+
+</style>
