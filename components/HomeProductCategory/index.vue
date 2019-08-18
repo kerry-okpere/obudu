cedc5095c807d680a5abee28e8c668ffdb89bfe5
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-08-18 19:07:03 +0100

    Removed duplicate div

--- components/HomeProductCategory/index.vue
+++ components/HomeProductCategory/index.vue
@@ -1,36 +1,36 @@
 <template>
-  <div class="container">
+  <div class="container pt-50">
     <div class="row">
       <div class="col-md-4 animated slideInUp">
-          <div class="single-banner mb-30 scroll-zoom" data-sr-id="2">
-            <a href="/product/">
+        <div class="single-banner mb-30 scroll-zoom" data-sr-id="2">
+          <a href="/product/">
             <span class="home-category__title">Clothing</span>
             <span class="home-category__overlay"></span>
             <img class="animated" src="@/assets/img/collections/image-2.jpg" alt="">
-            </a>
+          </a>
         </div>
-      <div class="col-md-4 animated slideInUp">
+        </div>
+        <div class="col-md-4 animated slideInUp">
           <div class="single-banner mb-30 scroll-zoom" data-sr-id="2">
             <a href="/product/">
               <span class="home-category__title">Accessories</span>
-                           <span class="home-category__overlay"></span>
+              <span class="home-category__overlay"></span>
               <img class="animated" src="@/assets/img/collections/image-3.jpg" alt="">
             </a>
           </div>
         </div>
-      <div class="col-md-4 animated slideInUp">
+        <div class="col-md-4 animated slideInUp">
           <div class="single-banner mb-30 scroll-zoom" data-sr-id="2">
             <a href="/product/">
               <span class="home-category__title">Accessories</span>
-                           <span class="home-category__overlay"></span>
+              <span class="home-category__overlay"></span>
               <img class="animated" src="@/assets/img/collections/image-2.jpg" alt="">
             </a>
           </div>
         </div>
-            </div>
-      
-<!-- >> -->
-  <!-- <div class="featured-category__section w-container">
+
+      <!-- >> -->
+      <!-- <div class="featured-category__section w-container">
     <div class="w-layout-grid grid">
       <div v-for="category in categories" :key="category.node.id" id="w-node-995953f3b94f-3cdbab3b" class="featured-category__box">
           <div class="featured-category__gridbox">
@@ -56,13 +56,16 @@
           </a>
           </div>
       </div> -->
-    <!-- </div>
+      <!-- </div>
     >>> -->
+      </div>
   </div>
 </template>
 
 <script>
-import { GET_HOMEPAGE_CATEGORIES } from "../../queries/productQueries";
+  import {
+    GET_HOMEPAGE_CATEGORIES
+  } from "../../queries/productQueries";
 
   export default {
     name: "HomeProductCategory",
@@ -73,7 +76,7 @@ import { GET_HOMEPAGE_CATEGORIES } from "../../queries/productQueries";
       }
     },
 
-    async mounted(){
+    async mounted() {
       let categories = await this.getCategories();
       categories.pop();
       this.categories = categories;
@@ -83,19 +86,19 @@ import { GET_HOMEPAGE_CATEGORIES } from "../../queries/productQueries";
     methods: {
       async getCategories() {
         let response = await this.$apollo.query({
-            query: GET_HOMEPAGE_CATEGORIES
+          query: GET_HOMEPAGE_CATEGORIES
         });
 
         return response.data.categories.edges;
-        
+
       }
     }
 
   }
 
-
 </script>
 
 <style lang="scss" scoped>
   @import "./_index.scss";
+
 </style>
