288f534c2dbc1221477f30f4ba51376aa55cc7a5
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-08-18 19:00:45 +0100

    Added checkout page

---
+++ pages/checkout/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <SectionBreadcrumbs pageTitle="Checkout" />
+    <CheckoutForm />
+  </div>
+</template>
+
+<script>
+
+import SectionBreadcrumbs from '../../components/SectionBreadcrumbs';
+import CheckoutForm from '../../components/CheckoutForm/index';
+
+export default {
+    components: {
+        CheckoutForm,
+        SectionBreadcrumbs
+    }
+
+}
+</script>
+
+<style>
+
+</style>
