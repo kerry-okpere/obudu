034272d1f06335c356dba1be10d846e5ac8bc98b
enkay <ogbulu@mercurie.ng>
2019-09-10 10:24:11 +0100

    persisting store currency as well yikes :see_no_evil:

--- plugins/vuex-persist.js
+++ plugins/vuex-persist.js
@@ -3,7 +3,10 @@ import VuexPersistence from 'vuex-persist'
 export default ({ store }) => {
     window.onNuxtReady(() => {
       new VuexPersistence({
-        reducer: (state) => ({ cart: state.cart }),
+        reducer: (state) => ({ 
+          cart: state.cart,
+          currency: state.currency
+        }),
       }).plugin(store);
     });
   }
