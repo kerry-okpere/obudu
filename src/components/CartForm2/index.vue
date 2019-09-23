89e88c89c48cc0d7c740467ba6dbd0907eb6c5e8
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-09-23 12:29:40 +0100

    Modified cart form component

--- src/components/CartForm2/index.vue
+++ src/components/CartForm2/index.vue
@@ -3,6 +3,7 @@
     <div class="container">
       <div class="row">
         <div class="col">
+          <div class="table-responsive">
           <table class="cart-table">
             <thead>
               <tr>
@@ -10,57 +11,80 @@
                 <th></th>
                 <th>Price</th>
                 <th>Quantity</th>
-                <th>Total</th>
+                <th class="h-total">Total</th>
                 <th></th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td class="product-thumbnail"><img
-                    src="https://saleor-demo.s3.amazonaws.com/__sized__/products/saleordemoproduct_cl_boot07_1-thumbnail-510x510.png"
-                    width="100" ></td>
-                <td class="product-name">Apple Organic Juice</td>
-                <td class="product-price">$69</td>
+              <tr v-for="getCartItem in getCartItems" :key="`${getCartItem.prodId}`">
+                <td class="product-thumbnail">
+                  <a href="#"><img :src="`${getCartItem.imgUrl}`" alt=""></a>
+                </td>
+                <td class="product-name"><a href="#">{{getCartItem.prodName}} - {{getCartItem.name}}</a></td>
+                <td class="product-price">{{getCurrency}} {{getCartItem.price}}</td>
                 <td class="product-quantity">
-                  <div class="quantity">
+                  <span class="quantity">
                     <b-input-group>
                       <b-input-group-prepend>
                         <b-btn variant="outline-info" @click="num--">-</b-btn>
                       </b-input-group-prepend>
 
-                      <b-form-input type="number" min="0.00" v-model="num"></b-form-input>
+                      <b-form-input type="number" min="0.00" v-model="num" @change="onQtyChange(`${getCartItem.prodId}`)" ref="itemQty" :value="`${getCartItem.quantity}`"></b-form-input>
 
                       <b-input-group-append>
                         <b-btn variant="outline-secondary" @click="num++">+</b-btn>
                       </b-input-group-append>
                     </b-input-group>
-                  </div>
+                  </span>
                 </td>
-                <td class="product-subtotal">$69</td>
-                                <td class="product-remove">
+                <td class="product-subtotal" ref="totalPrice"v-on: :value="`${getCartItem.quantity * getCartItem.price}`">{{getCurrency}} {{ getCartItem.quantity * getCartItem.price }}>$69</td>
+                <td class="product-remove">
                   <ion-icon name="trash"></ion-icon>
                 </td>
               </tr>
             </tbody>
           </table>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-8 col-md-8 col-sm-12"></div>
+        <div class="col-lg-4 col-md-4 col-sm-12 c-button">
+          <b-button size="lg" variant="primary" type="submit">Proceed to checkout</b-button>
         </div>
       </div>
     </div>
   </section>
-
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        num: 0
-      }
+export default {
+  data() {
+    return {
+      num: 0
     }
-  }
+  },
+  computed: {
+        getCartItems(){
+            return this.$store.getters.getCartItems;
+        },
+        grandTotal(){
+            return this.$store.getters.cartTotalPrice
+        },
+        getCurrency(){
+            return this.$store.getters.getStoreCurrency
+        }
+    },
 
+    methods: {
+        onQtyChange(event){
+            this.$store.dispatch('incrementCartQuantity', event);
+        },
+        
+    }
+};
 </script>
 
 <style lang="scss" scoped>
-@import './_index.scss';
+@import "./_index.scss";
 </style>
