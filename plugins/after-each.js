fb529b420589867bc78ed25c2f9e261c0178a325
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-12-17 15:04:30 +0100

    Clearing out repo

--- plugins/after-each.js
+++
@@ -1,5 +0,0 @@
-export default ({ app }) => {
-    app.router.afterEach((to, from) => {
-              $(document).trigger('router-loaded');
-    })
-  }
--- plugins/aos.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
-Vue.use(AOS);
-AOS.init()
