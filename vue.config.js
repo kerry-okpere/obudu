8e54c389093a40b425b7bd38809d74bd1601a6a2
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-12-18 11:26:59 +0100

    Added antdv global variables

--- vue.config.js
+++ vue.config.js
@@ -16,5 +16,17 @@ module.exports = {
     types.forEach(type => {
       addStyleResource(config.module.rule('scss').oneOf(type))
     })
-	}
+  },
+  // css: {
+  //   loaderOptions: {
+  //     less: {
+  //       modifyVars: {
+  //         'primary-color': '#1DA57A',
+  //         'link-color': '#1DA57A',
+  //         'border-radius-base': '2px',
+  //       },
+  //       javascriptEnabled: true
+  //     }
+  //   }
+  // }
 }
