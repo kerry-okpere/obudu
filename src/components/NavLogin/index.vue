67b9ff0c0d88e291a118afad681ddee4bb7e111c
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2019-12-18 13:21:52 +0100

    Added new components

---
+++ src/components/NavLogin/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="login">
+        <a-tabs defaultActiveKey="1" @change="callback">
+            <a-tab-pane tab="Tab 1" key="1">Content of Tab Pane 1</a-tab-pane>
+            <a-tab-pane tab="Tab 2" key="2" forceRender>Content of Tab Pane 2</a-tab-pane>
+            <a-tab-pane tab="Tab 3" key="3">Content of Tab Pane 3</a-tab-pane>
+        </a-tabs>
+    </section>
+</template>
+
+<script>
+export default {
+    methods: {
+        callback(key) {
+            console.log(key);
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+@import './_index';
+</style>
