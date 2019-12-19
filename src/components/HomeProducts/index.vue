5fe427c2988ca15f40398dcd5e30306311597d7b
enkaypeter <enkaypeter@gmail.com>
2019-12-19 07:04:22 +0100

    fixing  is undefined in Homeproducts component

--- src/components/HomeProducts/index.vue
+++ src/components/HomeProducts/index.vue
@@ -8,7 +8,7 @@
                     <div class="col" v-for="(product, index) in products" :key="index" >
                         <a-card :hoverable="false" @mouseover="hover = true" @mouseleave="hover = false" :bordered="true" :loading="loading" style="width: 240px">
                             <img alt="example" :src="`${product.images[0].url}`" width="240" />
-                            <a-button type="primary" v-bind:style="{backgroundColor: this.$store.state.styles.priColor, borderColor: this.$store.state.styles.priColor}">Buy Now</a-button>
+                            <a-button type="primary" v-bind:style="{backgroundColor: priColor, borderColor: priColor}">Buy Now</a-button>
                             <div class="products__one-meta">
                                 <h2>{{product.name}}</h2>
                                 <h3>{{product.categoryName}}</h3>
@@ -27,7 +27,7 @@
                     <div class="col-6 col-xl-3 col-lg-3 col-md-4 col-sm-6">
                         <a-card :hoverable="false" @mouseover="hover = true" @mouseleave="hover = false" :bordered="true" :loading="loading" style="width: 240px">
                             <img alt="example" src="https://cdn.shopify.com/s/files/1/1724/2825/products/IMG_8191_250x.jpg?v=1553005631" width="240" />
-                            <a-button type="primary" v-bind:style="{backgroundColor: this.$store.state.priColor, borderColor: this.$store.state.priColor}">Buy Now</a-button>
+                            <a-button type="primary" v-bind:style="{backgroundColor: priColor, borderColor: priColor}">Buy Now</a-button>
                             <div class="products__one-meta">
                                 <h2>Product Name</h2>
                                 <h3>Product Category</h3>
@@ -38,7 +38,7 @@
                     <div class="col-6 col-xl-3 col-lg-3 col-md-4 col-sm-6">
                         <a-card :hoverable="false" @mouseover="hover = true" @mouseleave="hover = false" :bordered="true" :loading="loading" style="width: 240px">
                             <img alt="example" src="https://cdn.shopify.com/s/files/1/1724/2825/products/IMG_8191_250x.jpg?v=1553005631" width="240" />
-                            <a-button type="primary" v-bind:style="{backgroundColor: this.$store.state.priColor, borderColor: this.$store.state.priColor}">Buy Now</a-button>
+                            <a-button type="primary" v-bind:style="{backgroundColor: priColor, borderColor: priColor}">Buy Now</a-button>
                             <div class="products__one-meta">
                                 <h2>Product Name</h2>
                                 <h3>Product Category</h3>
@@ -49,7 +49,7 @@
                     <div class="col-6 col-xl-3 col-lg-3 col-md-4 col-sm-6">
                         <a-card :hoverable="false" @mouseover="hover = true" @mouseleave="hover = false" :bordered="true" :loading="loading" style="width: 240px">
                             <img alt="example" src="https://cdn.shopify.com/s/files/1/1724/2825/products/IMG_8191_250x.jpg?v=1553005631" width="240" />
-                            <a-button type="primary" v-bind:style="{backgroundColor: this.$store.state.priColor, borderColor: this.$store.state.priColor}">Buy Now</a-button>
+                            <a-button type="primary" v-bind:style="{backgroundColor: priColor, borderColor: priColor}">Buy Now</a-button>
                             <div class="products__one-meta">
                                 <h2>Product Name</h2>
                                 <h3>Product Category</h3>
@@ -60,7 +60,7 @@
                     <div class="col-6 col-xl-3 col-lg-3 col-md-4 col-sm-6">
                         <a-card :hoverable="false" @mouseover="hover = true" @mouseleave="hover = false" :bordered="true" :loading="loading" style="width: 240px">
                             <img alt="example" src="https://cdn.shopify.com/s/files/1/1724/2825/products/IMG_8191_250x.jpg?v=1553005631" width="240" />
-                            <a-button type="primary" v-bind:style="{backgroundColor: this.$store.state.priColor, borderColor: this.$store.state.priColor}">Buy Now</a-button>
+                            <a-button type="primary" v-bind:style="{backgroundColor: priColor, borderColor: priColor}">Buy Now</a-button>
                             <div class="products__one-meta">
                                 <h2>Product Name</h2>
                                 <h3>Product Category</h3>
@@ -75,7 +75,8 @@
 </template>
 
 <script>
-import { mapGetters, mapMutations } from 'vuex'
+import { mapGetters } from 'vuex'
+
 
     export default {
         data: () => ({
