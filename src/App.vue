ef9090900e6a6b64bc338063452cc56326ef3e7d
Tim <timothyolaleke@gmail.com>
2020-01-23 10:38:46 +0100

    Fetch styles on load

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
-    <div id="app">
-        <router-view></router-view>
-    </div>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
+
+<script>
+import { STORENAME } from "./config";
+
+export default {
+  methods: {
+    capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+  },
+  async created() {
+    let res = await this.$store.dispatch("products/fetchStoreStyles");
+    let savedStyles = { ...res, storeName: this.capitalizeFirstLetter(STORENAME) };
+    let mergedStyles = { ...this.$store.state.styles, ...savedStyles };
+    this.$store.commit("updateStyles", mergedStyles);
+  }
+};
+</script>
