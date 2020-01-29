d762a75a5a0d87970fec33bea28b1a761e84be71
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2020-01-29 13:39:54 +0100

    Removed unhosted images

--- src/components/ThemeSettings/components/ThemePicker.vue
+++ src/components/ThemeSettings/components/ThemePicker.vue
@@ -3,7 +3,7 @@
     <div class="row text-center">
       <div class="col-6 col-xl-4">
         <a-card hoverable :style="custom">
-          <img src="https://demo.hasthemes.com/shopify/lezada/assets/images/home-preview/watch-1.jpg" slot="cover"/>
+          <img src="@/assets/img/preview/sample-1.jpg" slot="cover"/>
           <a-card-meta title="Jupiter">
             <template slot="description">Minimalist theme for your store</template>
           </a-card-meta>
@@ -12,7 +12,7 @@
       </div>
       <div class="col-6 col-xl-4">
         <a-card hoverable :style="custom">
-          <img src="https://demo.hasthemes.com/shopify/lezada/assets/images/home-preview/shoe-1.jpg" slot="cover"/>
+          <img src="@/assets/img/preview/sample-3.jpg" slot="cover"/>
           <a-card-meta title="Venus">
             <template slot="description">Modern theme for your store</template>
           </a-card-meta>
@@ -21,7 +21,7 @@
       </div>
       <div class="col-6 col-xl-4">
         <a-card hoverable :style="custom">
-          <img src="https://demo.hasthemes.com/shopify/lezada/assets/images/home-preview/food-1.jpg" slot="cover"/>
+          <img src="@/assets/img/preview/sample-3.jpg" slot="cover"/>
           <a-card-meta title="Mars">
             <template slot="description">Clean theme for your store</template>
           </a-card-meta>
