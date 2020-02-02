cdace28f95f91a99995999f7dfb66ecf86147316
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2020-02-02 05:59:20 +0100

    Committing is hard

---
+++ src/components/HomeProductsQuickView/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="products__view">
+    <div class="row">
+      <div class="col-12 col-xl-6">
+        <ProductImage />
+      </div>
+      <div class="col-12 col-xl-6">
+        <ProductBreadcrumb />
+        <ProductInfo />
+      </div>  
+    </div>
+  </section>
+</template>
+
+<script>
+import ProductImage from '@/components/ProductImage';
+import ProductBreadcrumb from '@/components/ProductBreadcrumb';
+import ProductInfo from '@/components/ProductInfo';
+
+export default {
+  components: {
+    ProductImage,
+    ProductBreadcrumb,
+    ProductInfo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './index';
+</style>
