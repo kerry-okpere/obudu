7969f13d405610aa185bd548763921b244d1a3b1
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2020-02-19 16:26:33 +0100

    Modified products page

--- src/components/ProductInfo/index.vue
+++ src/components/ProductInfo/index.vue
@@ -5,16 +5,22 @@
       <!-- <h1>{{ selectedVariant }}</h1> -->
       <!-- <a-rate v-model="rating" />
       <h4>(2 ratings)</h4> -->
-      <h2>&#x20A6;{{ formatPrice(selectedVariant.price || singleProd.basePrice) }}</h2>
       <!-- {{singleProds}} -->
     </div>
     <div class="product__info-description">
+      <h3 class="m-0">{{singleProd.categoryName}}</h3>
+    </div>
+    <div class="product__info-description">
+      <h3 class="pb-1">Description</h3>
       <p>{{singleProd.description}}</p>
     </div>
-
+    <div class="product__info-price">
+      <h3>Price</h3>
+      <h2>&#x20A6;{{ formatPrice(selectedVariant.price || singleProd.basePrice) }}</h2>
+    </div>
     <div class="product__info-variant">
       <div v-for="(va, index) in attrType" :key="index">
-        <h4>{{ va }}</h4>
+        <h3 class="pb-2">{{ va }}</h3>
         <a-radio-group @change="onChange" :v-model="value">
           <a-radio-button
             v-for="(value, index) in attrVal[va]"
@@ -26,14 +32,14 @@
     </div>
 
     <div class="product__info-quantity">
-      <h4>Quantity</h4>
+      <h3 class="pb-2">Quantity</h3>
       <a-input-number :min="1" :max="10" v-model="value" @change="changeQuant" />
     </div>
     <div class="product__info-cart">
-      <s-button v-if="!selectedVariant" :pri="priColor" :sec="secColor" class="disabled">Add to Cart</s-button>
-      <s-button v-else :pri="priColor" :sec="secColor" @click="addToCart(selectedVariant)"><a-icon type="plus" class="pr-2" />Add to Cart</s-button>
-      <!-- <a-button v-if="!selectedVariant" :size="btnSize" block disabled>Add to Cart</a-button> -->
-      <!-- <a-button v-else type="primary" icon="plus" :size="btnSize" @click="addToCart(selectedVariant)" block >Add to Cart</a-button> -->
+      <s-button v-if="!selectedVariant" :pri="priColor" :sec="secColor" class="disabled mb-0">Add to Cart</s-button>
+      <s-button v-else :pri="priColor" :sec="secColor" @click="addToCart(selectedVariant)" class="mb-0"><a-icon type="plus" class="pr-2" />Add to Cart</s-button>
+      <s-button-outline v-if="!selectedVariant" :pri="priColor" :sec="secColor" class="disabled">Buy Now</s-button-outline>
+      <s-button-outline v-else :pri="priColor" :sec="secColor">Buy Now</s-button-outline>
     </div>
   </div>
 </template>
