91398a13cbc3e63abbf6a2f900421457e2829a88
Oluwatoni Atunrase <thatmusicjunkie@gmail.com>
2020-02-23 16:27:06 +0100

    Component restructuring

---
+++ src/components/NavHeader/components/NavAction.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="navbar__one-nav navbar-cta" style="margin: 6px 0 0;">
+    <div class="navbar__item" :style="{color: navColor}">
+    <!-- Cart -->
+    <a-badge :count="actualCartCount" :numberStyle="cartIconStyle" :showZero="false">
+      <a-button type="link" @click="setCartShow">
+        <v-icon name="shopping-cart" :style="{color: navColor}" />
+        <span :style="{color: navColor}">Cart</span>
+      </a-button>
+    </a-badge>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+  data: () => ({
+    userLoggedin: false,
+    cartItems: 0,
+    cartIconStyle: {backgroundColor: "#ff0909", borderColor: "#ff0909", marginTop: "4px"},
+    getCartCount: 0,
+  }),
+  computed: {
+    ...mapGetters([
+      'navColor',
+      'cartShow',
+      'loginShow'
+      
+    ]),
+    actualCartCount() {
+      return this.$store.getters["getCartQuantity"];
+    },
+  },
+  methods: {
+    setCartShow() {
+      this.$store.commit('setCartShow', true);
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NavHeader/components/NavSearch.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="navbar__one-search search" :style="{color: navColor}">
+        <a-input-search placeholder="Search store..." style="width: 250px;float: right;" />
+    </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex';
+export default {
+    computed: {
+        ...mapGetters([
+            'navColor'
+        ])
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../index';
+</style>
